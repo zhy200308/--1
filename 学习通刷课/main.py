6c6dae2be08077e5c773e65c6c30d5e8581b905a
Levitans <77911531+Levitans@users.noreply.github.com>
2021-11-09 12:44:40 +0800

    Add files via upload

--- 学习通刷课/main.py
+++ 学习通刷课/main.py
@@ -4,34 +4,46 @@
 # @File : main.py
 # @Software : PyCharm
 
+
 import os
 import sys
+import requests
+import json
 from package.user import User
 from package.ControlWeb.xueXiTong import XueXiTong
 from package.disPlay import DisPlay
 from package.manageDate import UserData
 from package.manageDate import SubjectData
+from package.manageDate import BrowserConfiguration
+
+starInformation = """
+===========================================
+作者：Levitan
+GitHub地址：https://github.com/Levitans/-
+===========================================
+"""
+print(starInformation)
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-
-driverPath = resource_path(r"driver\chrome\chromedriver.exe")
-chromePath = resource_path(r"driver\chrome\chrome.exe")
-
+# 地址初始化
 nowPath = os.getcwd()
+driverPath = "{}\\driver\\chrome\\chromedriver.exe".format(nowPath)
+chromePath = "{}\\driver\\chrome\\chrome.exe".format(nowPath)
 userDataPath = "{}\\data\\user_data.json".format(nowPath)
 subjectDataPath = "{}\\data\\subject_data.json".format(nowPath)
-
+browserConfigurationPath = "{}\\data\\browser_configuration.json".format(nowPath)
 
 userData = UserData(userDataPath)
-
-while True:
-    print("选择模式：")
+browser = BrowserConfiguration(browserConfigurationPath)
+while True and isExpiration:
+    browserInf = "关闭浏览器显示" if browser.getState() == 1 else "开启浏览器显示"
+    print("选择模式（{}）".format(browserInf))
     function = "1、创建新用户，2、使用已有用户（当前已有{}个用户）" \
-               "，3、修改用户信息，4、退出程序\n输入序号：".format(userData.getUserAmount())
+               "，3、修改用户信息，4、设置浏览器显示，5、退出程序\n输入序号：".format(userData.getUserAmount())
     mode = input(function)
     print()
     while not (mode in ("1", "2", "3", "4", "5", "6")):
@@ -49,15 +61,16 @@ while True:
         for j, i in enumerate(userData.getUsers()):
             print(str(j+1)+"、"+i.getUserName())
         userIndex = int(input("选择用户：")) - 1
-        print()
+        DisPlay.displayPart()
         user = userData.getUsers()[userIndex]
-        xueXiTong = XueXiTong(chromePath, driverPath, user)
+        xueXiTong = XueXiTong(chromePath, driverPath, user, browser.getState())
         xueXiTong.landing()
         # 获取课程列表
         coursesList = xueXiTong.getCourses()
         # 展示课程
-        DisPlay.disPlay(coursesList)
+        DisPlay.display(coursesList)
         courseIndex = int(input("输入课程号：")) - 1
+
         # 进入指定课程
         # 返回课程中的章节的列表
         chaptersList = xueXiTong.enterCourse(courseIndex)
@@ -80,6 +93,17 @@ while True:
             userData.modifyUserData(user.getUserName(), newData, "account")
         elif key == 2:
             userData.modifyUserData(user.getUserName(), newData)
+    elif mode == "4":
+        key = input("1、关闭浏览器显示，2、开启浏览器显示\n输入序号：")
+        if key == "1":
+            browser.modifyClassData(1)
+            print("已关闭显示浏览器\n")
+        elif key == "2":
+            browser.modifyClassData(0)
+            print("已开启显示浏览器\n")
+        else:
+            print("输入错误\n")
     else:
         break
 os.system('pause')
+
